d1afff4b9a18bb3923fa538aecddabe5c3026857
nguyennhut <nhut.nguyen@xomad.com>
2024-01-26 16:59:42 +0700

    initial commit

---
+++ apps/xomad-doc/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'web-core-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'xomad-doc';
+}
